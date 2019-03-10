47eadd3434ca5f7ddb85495f0ec8f79ca65c31eb
mulenokd <mulenokd@gmail.com>
2019-03-10 23:52:31 +0700

    123

---
+++ js/main.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){	
+    function resize(){
+       if( typeof( window.innerWidth ) == 'number' ) {
+            myWidth = window.innerWidth;
+            myHeight = window.innerHeight;
+        } else if( document.documentElement && ( document.documentElement.clientWidth || 
+        document.documentElement.clientHeight ) ) {
+            myWidth = document.documentElement.clientWidth;
+            myHeight = document.documentElement.clientHeight;
+        } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+            myWidth = document.body.clientWidth;
+            myHeight = document.body.clientHeight;
+        }
+    }
+    $(window).resize(resize);
+    resize();
+
+    $.fn.placeholder = function() {
+        if(typeof document.createElement("input").placeholder == 'undefined') {
+            $('[placeholder]').focus(function() {
+                var input = $(this);
+                if (input.val() == input.attr('placeholder')) {
+                    input.val('');
+                    input.removeClass('placeholder');
+                }
+            }).blur(function() {
+                var input = $(this);
+                if (input.val() == '' || input.val() == input.attr('placeholder')) {
+                    input.addClass('placeholder');
+                    input.val(input.attr('placeholder'));
+                }
+            }).blur().parents('form').submit(function() {
+                $(this).find('[placeholder]').each(function() {
+                    var input = $(this);
+                    if (input.val() == input.attr('placeholder')) {
+                        input.val('');
+                    }
+                });
+            });
+        }
+    }
+    $.fn.placeholder();
+
+    // $(".b-step-slider").slick({
+    //     dots: true,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     infinite: true,
+    //     cssEase: 'ease', 
+    //     speed: 500,
+    //     arrows: true,
+    //     prevArrow: '<button type="button" class="slick-prev slick-arrow icon-arrow-left"></button>',
+    //     nextArrow: '<button type="button" class="slick-next slick-arrow icon-arrow-right"></button>',
+    //     touchThreshold: 100
+    // });
+
+    // // Первая анимация элементов в слайде
+    // $(".b-step-slide[data-slick-index='0'] .slider-anim").addClass("show");
+
+    // // Кастомные переключатели (тумблеры)
+    // $(".b-step-slider").on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    //     $(".b-step-tabs li.active").removeClass("active");
+    //     $(".b-step-tabs li").eq(nextSlide).addClass("active");
+    // });
+
+    // // Анимация элементов в слайде
+    // $(".b-step-slider").on('afterChange', function(event, slick, currentSlide, nextSlide){
+    //     $(".b-step-slide .slider-anim").removeClass("show");
+    //     $(".b-step-slide[data-slick-index='"+currentSlide+"'] .slider-anim").addClass("show");
+    // });
+
+
+    
+	// var myPlace = new google.maps.LatLng(55.754407, 37.625151);
+ //    var myOptions = {
+ //        zoom: 16,
+ //        center: myPlace,
+ //        mapTypeId: google.maps.MapTypeId.ROADMAP,
+ //        disableDefaultUI: true,
+ //        scrollwheel: false,
+ //        zoomControl: true
+ //    }
+ //    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); 
+
+ //    var marker = new google.maps.Marker({
+	//     position: myPlace,
+	//     map: map,
+	//     title: "Ярмарка вакансий и стажировок"
+	// });
+
+});
